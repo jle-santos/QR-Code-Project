3c2839b950053fef859128c7942e3e2b5d94fab5
jle-santos <49331367+jle-santos@users.noreply.github.com>
2019-11-24 15:18:37 -0800

    Alpha Build - Working Detection

--- Functions/SampleInterval.m
+++ Functions/SampleInterval.m
@@ -1,14 +1,38 @@
-function AC = SamplingInterval(A, C, NumSamples)
+function SamplingVector = SampleInterval(A, C, NumSample)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+
+    SamplingVector = zeros(2,11);
+    Factor = 1.10;
+    
     AC = struct;
     AC.diff = norm(A - C);
-    AC.dY = A(2) - C(2);
-    AC.dX = A(1) - C(1);
+    AC.dY = C(2) - A(2);
+    AC.dX = C(1) - A(1);
     AC.dPhi = atand(AC.dY/AC.dX);
-    AC.dStep = AC.diff/NumSamples;
-    AC.dSampleY = abs(AC.dStep*sind(AC.dPhi));
-    AC.dSampleX = abs(AC.dStep*cosd(AC.dPhi));
+    AC.dStep = AC.diff/NumSample;
     
+    if AC.dY < 0
+        AC.dSampleY = (-1)*abs(AC.dStep*sind(AC.dPhi));
+    else
+        AC.dSampleY = abs(AC.dStep*sind(AC.dPhi));
+    end
+    
+    if AC.dX < 0
+        AC.dSampleX = (-1)*abs(AC.dStep*cosd(AC.dPhi));
+    else
+        AC.dSampleX = abs(AC.dStep*cosd(AC.dPhi));
+    end
+    
+    %Create sampling vectors
+    %StartPoint
+    
+    for Sample = 0:NumSample-1
+        PointAC.x = round(A(1) + AC.dSampleX*Sample*Factor);
+        PointAC.y = round(A(2) + AC.dSampleY*Sample*Factor);
+        
+        SamplingVector(1,Sample+1) = PointAC.x;
+        SamplingVector(2,Sample+1) = PointAC.y;
+    end
 end
 
